47939b35df10a7521c52699e3edfceb9f5d6a91f
Ernesto Rivera <53798130+netor82@users.noreply.github.com>
2025-10-06 22:57:46 -0600

    ChooseItem grid layout

--- src/turnopos.client/src/components/order/ChooseItem.tsx
+++ src/turnopos.client/src/components/order/ChooseItem.tsx
@@ -3,11 +3,12 @@ import type Item from '../../models/Item';
 
 export interface ChooseItemProps {
     items: Item[];
+    selectDirectory?: boolean;
     onSelect: (x: Item) => void;
     onCancel: () => void;
 }
 
-const ChooseItem: React.FC<ChooseItemProps> = ({ items, onSelect, onCancel }) => {
+const ChooseItem: React.FC<ChooseItemProps> = ({ items, onSelect, onCancel, selectDirectory }) => {
     const [selectedItem, setSelectedItem] = useState<Item | null>(null);
     const [search, setSearch] = useState('');
 
@@ -20,7 +21,14 @@ const ChooseItem: React.FC<ChooseItemProps> = ({ items, onSelect, onCancel }) =>
             setSelectedItem(item);
         }
     }
-    const children = items.filter(i => i.parentId == selectedItem?.id);
+    const handleSelectDirectory = () => {
+        if (selectedItem) {
+            onSelect(selectedItem);
+            setSelectedItem(null);
+        }
+    }
+
+    const children = items.filter(i => i.parentId == selectedItem?.id && (!selectDirectory || i.isDirectory));
 
     const handleKeyUp = (e: React.KeyboardEvent) => {
         const key = +e.key;
@@ -51,20 +59,36 @@ const ChooseItem: React.FC<ChooseItemProps> = ({ items, onSelect, onCancel }) =>
         onCancel();
     }
 
-    const backButton = selectedItem == null ? '' :
-        (<button key="0-back" onClick={goBack} >0 - ⏮️Atrás</button>)
+    const backButton = selectedItem == null ? <i></i> :
+        (
+            <button key="0-back" onClick={goBack} >
+                <span className="item-picker-number">0</span>
+                ⏮️Atrás
+            </button>
+        )
+    const renderItemButton = (child: Item, index: number) =>
+            <button key={child.id} onClick={() => handleSelectItem(child)} >
+                <span className="item-picker-number">{index + 1}</span>
+                <span className={'item-name' + (!child.isDirectory && !child.stock && ' strike' || '')}>{child.name}</span>
+                <span className="item-picker-stock">{child.isDirectory ? '' : child.stock}</span>
+            </button>;
+
+    const renderSelectDiretoryButton =
+        (selectDirectory && selectedItem && selectedItem.children && selectedItem.children.length < 9) ?
+            <button onClick={() => handleSelectDirectory()}>
+                Mover aquí
+            </button>
+            : null;
+    ;
 
 
     return (
-        <div onKeyUp={handleKeyUp}>
-            <input autoFocus type="text" value={search} readOnly />
-            <br />
+        <div onKeyUp={handleKeyUp} className="item-picker">
+            <input autoFocus type="text" value={search} readOnly placeholder="Usar el teclado" />
+            <span>{selectedItem?.name}</span>
             {backButton}
-            {children.map((child, index) => (
-                <button key={child.id} onClick={() => handleSelectItem(child)} >
-                    {index + 1} - <strong className={(!child.isDirectory && !child.stock && 'strike') || ''}>{child.name}</strong>
-                </button>
-            ))}
+            {children.map((child, index) => renderItemButton(child, index))}
+            {renderSelectDiretoryButton}
         </div>
     );
 }
